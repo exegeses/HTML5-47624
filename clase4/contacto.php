a53f862b62e206a78b5f559719024404a860e001
Marcos Pinardi <exegeses@gmail.com>
2021-02-27 16:42:25 -0300

    script de envío de formulario

---
+++ clase4/contacto.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Página de inicio</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+	<header></header>
+	<nav>
+		<a href="index.html">Inicio</a>
+		<a href="empresa.html">Empresa</a>
+		<a href="servicios.html">Servicios</a>
+		<a href="portfolio.html">Portfolio</a>
+		<a href="contacto.html">Contacto</a>
+	</nav>
+	<main>
+
+		<h1>Contacto</h1>
+
+	<?php 
+		//capturamos datos enviados por el form
+		$nombre = $_POST['nombre'];
+		$email = $_POST['email'];
+		$comentarios = $_POST['comentarios'];
+
+		//configuramos datos de email a enviar
+		$destinatario = 'dirección@email.com';
+		$asunto = 'Email enviado desde mi sitio';
+		$cuerpo = 'Nombre: '.$nombre.'<br>';
+		$cuerpo .= 'Email: '.$email.'<br>';
+		$cuerpo .= 'Comentarios: '.$comentarios;
+
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+		//enviamos el email
+		mail($destinatario, $asunto, $cuerpo, $headers);
+	?>
+
+		Gracias por su consulta, nos contactaremos a la brevedad.
+			
+
+	</main>
+	<footer>
+		© 1996-2020, Pandemic Warriors Inc.
+	</footer>
+	
+</body>
+</html>
